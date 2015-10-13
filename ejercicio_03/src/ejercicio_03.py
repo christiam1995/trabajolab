c96f3d8a38f2afab2476fb91a058e3457b46b243
Sergio <sergio.12.educando@gmail.com>
2015-10-13 11:26:32 -0500

    Final

---
+++ ejercicio_03/src/ejercicio_03.py
@@ -0,0 +1,11 @@
+i=1000
+while i<=1000 and i>0:
+ valor = input ("cantidad de entrega:" )
+
+ i=i-valor
+ if i<200:
+     print(" los suministros eesta bajo de las 200 unidades")
+     
+if i<0:
+    print("no hay suministros")
+    
